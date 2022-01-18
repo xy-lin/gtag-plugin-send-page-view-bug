e4b4258cb28baec7fd81f1d00a37fe881f1a186e
Xinyu Lin <xinyu.lin@siemens.com>
2022-01-18 15:45:17 +0000

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,31 @@
 module.exports = {
-    siteMetadata: {
-      title: `gtag plugin sent-page-view bug`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `gtag plugin sent-page-view bug`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-xxxxxx",
+          "AW-xxxxxx", // Google Ads / Adwords / AW
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          send_page_view: true,
+          cookie_expires: 123,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          respectDNT: true,
+        },
+      },
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
